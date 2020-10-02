afba3e31ab41d29de642b5bcb83ed0b377ffb42d
Aleks <osinaleksandr1989@gmail.com>
2020-10-02 11:54:46 +0500

    2.3 Добавит маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
 import Main from "../main/main.jsx";
+import Login from "../login/login.jsx";
+import Offer from "../offer/offer.jsx";
+import Favorites from "../favorites/favorites.jsx";
 
 const App = (props) => {
 
   const {totalOffers} = props;
 
   return (
-    <Main totalOffers={totalOffers}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main totalOffers={totalOffers}/>
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route exact path="/offer:id">
+          <Offer/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
