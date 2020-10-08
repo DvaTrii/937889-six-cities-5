332e3275ee71ec4f9c146fca232022d37beefd61
Aleks <osinaleksandr1989@gmail.com>
2020-10-08 10:44:38 +0500

    === Подключит компонент Header на страницу Избранное и Логин ===

--- src/components/pages/favorites-page/favorites-page.jsx
+++ src/components/pages/favorites-page/favorites-page.jsx
@@ -1,31 +1,12 @@
 import React from "react";
+import MainHeader from "../../main-header/main-header";
 
 const FavoritesPage = () => {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+
+      <MainHeader />
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
