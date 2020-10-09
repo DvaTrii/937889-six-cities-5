8666bf2e664b8870cab3c6469d688ec9085c7f70
Aleks <osinaleksandr1989@gmail.com>
2020-10-09 21:02:47 +0500

    === Исправт адрес главной страницы в Link ===

--- src/components/pages/favorites-page/favorites-page.jsx
+++ src/components/pages/favorites-page/favorites-page.jsx
@@ -139,7 +139,7 @@ const FavoritesPage = () => {
         </div>
       </main>
       <footer className="footer container">
-        <Link to={``} className="footer__logo-link">
+        <Link to={`/`} className="footer__logo-link">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"></img>
         </Link>
       </footer>
