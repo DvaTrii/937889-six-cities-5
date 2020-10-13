d60494dfbdecc32599226bf5ee4ea8ef1b343dd2
Aleks <osinaleksandr1989@gmail.com>
2020-10-13 11:10:02 +0500

    4.1.2 Установит leaflet, настроит webpack для работы со стилями leaflet

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ],
   },
