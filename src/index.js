81fe2b2b08f074e5cea66b46617e005c1445125f
Aleks <osinaleksandr1989@gmail.com>
2020-10-18 11:15:40 +0500

    5.1.5 Подключит redux к проекту

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
 import App from "./components/app/app.jsx";
 
+import {reducer} from "./store/reducer";
 import offers from "./mocks/offers.js";
 import {reviews} from "./mocks/reviews";
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      offers={offers}
-      reviews={reviews}
-    />,
+    <Provider store={store}>
+
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
