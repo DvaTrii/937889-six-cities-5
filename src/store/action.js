0e0c8a7739a5aa07598351750e756605690226c8
Aleks <osinaleksandr1989@gmail.com>
2020-10-18 17:20:47 +0500

    === Отрефакторит reducer & action ===

--- src/store/action.js
+++ src/store/action.js
@@ -1,7 +1,5 @@
 export const ActionType = {
   SET_CURRENT_CITY: `SET_CURRENT_CITY`,
-  GET_CURRENT_CITY_OFFERS: `GET_CURRENT_CITY_OFFERS`,
-  RETURN_TO_MAINPAGE: `RETURN_TO_MAINPAGE`
 };
 
 export const ActionCreator = {
@@ -9,11 +7,4 @@ export const ActionCreator = {
     type: ActionType.SET_CURRENT_CITY,
     payload: currentCityName
   }),
-  getCurrentCityOffers: (offers, currentCityName) => ({
-    type: ActionType.GET_CURRENT_CITY_OFFERS,
-    payload: offers.filter((it) => it.city === currentCityName)
-  }),
-  returnToMainPage: () => ({
-    type: ActionType.RETURN_TO_MAINPAGE
-  })
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,6 @@ import {ActionType} from "./action";
 
 const InitialState = {
   city: `Amsterdam`,
-  currentCityOffers: []
 };
 
 const reducer = (state = InitialState, action) => {
@@ -14,16 +13,6 @@ const reducer = (state = InitialState, action) => {
       return extend(state, {
         city: currentCity,
       });
-
-    case ActionType.GET_CURRENT_CITY_OFFERS:
-      let currentCityOffers = action.payload;
-
-      return extend(state, {
-        currentCityOffers
-      });
-
-    case ActionType.RETURN_TO_MAINPAGE:
-      return extend(state, InitialState);
   }
 
   return state;
