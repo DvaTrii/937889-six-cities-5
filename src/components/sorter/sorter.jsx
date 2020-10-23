9a158e501223d54a07fc0a8a318794a8481af269
Aleks <osinaleksandr1989@gmail.com>
2020-10-23 21:23:00 +0500

    6.1.4 Adds hoc to Sorter

--- src/components/sorter/sorter.jsx
+++ src/components/sorter/sorter.jsx
@@ -2,68 +2,54 @@ import React from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 
+import {withActiveFlag} from "../hocs/withActiveFlag/withActiveFlag";
 import {SorterType, SorterActiveClass, SorterListOpenedClass} from "../../const";
 import {ActionCreator} from "../../store/action";
 
-class Sorter extends React.PureComponent {
-  constructor(props) {
-    super(props);
 
-    this.setActiveSorter = this.props.setActiveSorter;
+const Sorter = ({isActive, onActiveChange, activeSorter, setActiveSorter}) => {
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-type"
+        tabIndex="0"
+        onClick={onActiveChange}>
+        {activeSorter}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className={`places__options places__options--custom
+        ${isActive ?
+      SorterListOpenedClass.OPENED : ``}`}>
 
-    this.state = {
-      isOpened: false,
-    };
+        {
+          Object.values(SorterType).map((it) => (
+            <li key={it}
+              className={`places__option ${activeSorter === it ? SorterActiveClass.ACTIVE : ``}`}
+              onClick={() => {
+                setActiveSorter(it);
+                onActiveChange();
+              }}
+              tabIndex="0">
+              {it}
+            </li>))
+        }
 
-    this._handleClick = this._handleClick.bind(this);
-    this.setActiveSorter = this.setActiveSorter.bind(this);
-  }
-
-  _handleClick() {
-    this.setState({
-      isOpened: !this.state.isOpened
-    });
-  }
-
-  render() {
-    return (
-      <form className="places__sorting" action="#" method="get">
-        <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type"
-          tabIndex="0"
-          onClick={this._handleClick}>
-          {this.props.activeSorter}
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select"></use>
-          </svg>
-        </span>
-        <ul className={`places__options places__options--custom
-        ${this.state.isOpened ? SorterListOpenedClass.OPENED : ``}`}>
-          {
-            Object.values(SorterType).map((it) => (
-              <li key={it}
-                className={`places__option ${this.props.activeSorter === it ? SorterActiveClass.ACTIVE : ``}`}
-                onClick={() => {
-                  this.setActiveSorter(it);
-                  this._handleClick();
-                }}
-                tabIndex="0">
-                {it}
-              </li>))
-          }
-        </ul>
-        {/* <select className="places__sorting-type" id="places-sorting">*/}
-        {/*  <option className="places__option" value="popular" selected="">Popular</option>*/}
-        {/*  <option className="places__option" value="to-high">Price: low to high</option>*/}
-        {/*  <option className="places__option" value="to-low">Price: high to low</option>*/}
-        {/*  <option className="places__option" value="top-rated">Top rated first</option>*/}
-        {/* </select>*/}
-      </form>
-    );
-  }
-}
+      </ul>
+      {/* <select className="places__sorting-type" id="places-sorting">*/}
+      {/*  <option className="places__option" value="popular" selected="">Popular</option>*/}
+      {/*  <option className="places__option" value="to-high">Price: low to high</option>*/}
+      {/*  <option className="places__option" value="to-low">Price: high to low</option>*/}
+      {/*  <option className="places__option" value="top-rated">Top rated first</option>*/}
+      {/* </select>*/}
+    </form>
+  );
+};
 
 Sorter.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  onActiveChange: PropTypes.func.isRequired,
   activeSorter: PropTypes.string.isRequired,
   setActiveSorter: PropTypes.func.isRequired
 };
@@ -81,4 +67,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export {Sorter};
-export default connect(mapStateToProps, mapDispatchToProps)(Sorter);
+export default withActiveFlag(connect(mapStateToProps, mapDispatchToProps)(Sorter));
