90c9a4390cb5659f8d8b675ac810274b5b056b8e
Aleks <osinaleksandr1989@gmail.com>
2020-10-29 18:15:53 +0500

    === Fixes mock nearOffers ===

--- src/components/offer-detailed/offer-detailed.jsx
+++ src/components/offer-detailed/offer-detailed.jsx
@@ -5,7 +5,9 @@ import OfferAmenitiesList from "../offer-amenities-List/offer-amenities-list";
 import OfferReviewsList from "../offer-reviews-list/offer-reviews-list";
 import Map from "../map/map";
 
-const OfferDetailed = ({offer, reviews, nearOffers}) => {
+import nearOffers from "../../mocks/offers";
+
+const OfferDetailed = ({offer, reviews}) => {
 
   const {pictures, isPremium, isBookmark, price, title, type, rating, description, bedroomsMax,
     guestsMax, amenities, hostInfo: {avatar, name, isSuper}} = offer;
@@ -100,7 +102,7 @@ OfferDetailed.propTypes = {
     id: PropTypes.number.isRequired,
     pictures: PropTypes.array.isRequired,
     isPremium: PropTypes.bool.isRequired,
-    isBookmark: PropTypes.bool.isRequired,
+    isBookmark: PropTypes.bool,
     price: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
@@ -116,7 +118,6 @@ OfferDetailed.propTypes = {
     })
   }),
   reviews: PropTypes.array.isRequired,
-  nearOffers: PropTypes.array.isRequired
 };
 
 export default OfferDetailed;
--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,16 +1,20 @@
 import {OfferType} from "../const";
 
-const offers = [
+const nearOffers = [
   {
     id: 101,
     city: {
       name: `Amsterdam`,
-      coordinates: [52.38333, 4.9]
+      coordinates: { // был [33, 33]
+        latitude: 52.38333,
+        longitude: 4.9
+      }
     },
     coordinates: {
       latitude: 52.3909553943508,
       longitude: 4.85309666406198
     },
+    previewImage: `img/apartment-02.jpg`,
     pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
     isPremium: true,
     isBookmark: true,
@@ -27,18 +31,21 @@ const offers = [
       name: `Ross`,
       isSuper: true,
     },
-    nearOffers: [102, 103, 104]
   },
   {
     id: 102,
     city: {
       name: `Amsterdam`,
-      coordinates: [52.38333, 4.9]
+      coordinates: { // был [33, 33]
+        latitude: 52.38333,
+        longitude: 4.9
+      }
     },
     coordinates: {
       latitude: 52.369553943508,
       longitude: 4.85309666406198
     },
+    previewImage: `img/apartment-02.jpg`,
     pictures: [`img/room.jpg`, `img/room.jpg`, `img/room.jpg`, `img/room.jpg`, `img/room.jpg`, `img/room.jpg`],
     isPremium: false,
     isBookmark: true,
@@ -55,18 +62,21 @@ const offers = [
       name: `Chandler`,
       isSuper: true,
     },
-    nearOffers: [101, 103, 104]
   },
   {
     id: 103,
     city: {
       name: `Amsterdam`,
-      coordinates: [52.38333, 4.9]
+      coordinates: { // был [33, 33]
+        latitude: 52.38333,
+        longitude: 4.9
+      }
     },
     coordinates: {
       latitude: 52.3909553943508,
       longitude: 4.929309666406198
     },
+    previewImage: `img/apartment-02.jpg`,
     pictures: [`img/apartment-01.jpg`, `img/apartment-01.jpg`, `img/apartment-01.jpg`, `img/apartment-01.jpg`, `img/apartment-01.jpg`, `img/apartment-01.jpg`],
     isPremium: false,
     isBookmark: false,
@@ -83,204 +93,7 @@ const offers = [
       name: `Monica`,
       isSuper: false,
     },
-    nearOffers: [102, 101, 104]
   },
-  {
-    id: 104,
-    city: {
-      name: `Amsterdam`,
-      coordinates: [52.38333, 4.9]
-    },
-    coordinates: {
-      latitude: 52.3809553943508,
-      longitude: 4.939309666406198
-    },
-    pictures: [`img/apartment-03.jpg`, `img/apartment-03.jpg`, `img/apartment-03.jpg`, `img/apartment-03.jpg`, `img/apartment-03.jpg`, `img/apartment-03.jpg`],
-    isPremium: true,
-    isBookmark: false,
-    price: 100,
-    title: `Beautiful & luxurious apartment at great location`,
-    type: OfferType.ROOM,
-    rating: 2,
-    description: `At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti`,
-    bedroomsMax: `2 Bedrooms`,
-    guestsMax: `Max 3 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Fridge`, `Washing machine`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-angelina.jpg`,
-      name: `Rachel`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 101]
-  },
-  {
-    id: 105,
-    city: {
-      name: `Paris`,
-      coordinates: [48.864716, 2.349014]
-    },
-    coordinates: {
-      latitude: 48.858093,
-      longitude: 2.294694
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Paris and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Paris ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [110]
-  },
-  {
-    id: 106,
-    city: {
-      name: `Cologne`,
-      coordinates: [50.935173, 6.953101]
-    },
-    coordinates: {
-      latitude: 50.941357,
-      longitude: 6.958307
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Cologne and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Cologne ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 104]
-  },
-  {
-    id: 107,
-    city: {
-      name: `Brussels`,
-      coordinates: [50.8505, 4.3488]
-    },
-    coordinates: {
-      latitude: 50.8439,
-      longitude: 4.35763
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Brussels and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Brussels ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 104]
-  },
-  {
-    id: 108,
-    city: {
-      name: `Hamburg`,
-      coordinates: [53.551086, 9.993682]
-    },
-    coordinates: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Hamburg and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Hamburg ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 104]
-  },
-  {
-    id: 109,
-    city: {
-      name: `Paris`,
-      coordinates: [51.233334, 6.783333]
-    },
-    coordinates: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Dusseldorf and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Dusseldorf ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 104]
-  },
-  {
-    id: 110,
-    city: {
-      name: `Paris`,
-      coordinates: [48.864716, 2.349014]
-    },
-    coordinates: {
-      latitude: 48.857805,
-      longitude: 2.295193
-    },
-    pictures: [`img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`, `img/apartment-02.jpg`],
-    isPremium: true,
-    isBookmark: true,
-    price: 100,
-    title: `Paris and stone place`,
-    type: OfferType.APARTMENT,
-    rating: 5,
-    description: `Paris ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium`,
-    bedroomsMax: `4 Bedrooms`,
-    guestsMax: `Max 5 adults`,
-    amenities: [`Wi-Fi`, `Heating`, `Kitchen`, `Parking`, `Flowers`, `Dishwasher`, `Towels`, `TV`],
-    hostInfo: {
-      avatar: `img/avatar-max.jpg`,
-      name: `Ross`,
-      isSuper: true,
-    },
-    nearOffers: [102, 103, 104]
-  }
 ];
 
-export default offers;
+export default nearOffers;
