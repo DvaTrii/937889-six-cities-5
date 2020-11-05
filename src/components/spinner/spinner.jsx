0e2267cf2d47f5775892ccfdb70cf85901766315
Aleks <osinaleksandr1989@gmail.com>
2020-11-05 16:19:19 +0500

    7.3.2 Impliments spinner

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+import './spinner.css';
+
+export const Spinner = () => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
