3097150b9e909b6c4f48538f1fda15e8b4f5b613
Aleks <osinaleksandr1989@gmail.com>
2020-11-11 19:17:36 +0500

    8.9 Impliments test for hocs

---
+++ src/components/hocs/withLoadFlag/with-load-flag.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {withLoadFlag} from "./with-load-flag";
+
+const MockComponent = () => {
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+};
+
+const MockComponentWrapped = withLoadFlag(MockComponent);
+
+it(`withLoadFlag is rendered correctly`, () => {
+  const tree = renderer.create((
+    <MockComponentWrapped isLoad={true}/>
+  ), {
+    createNodeMock() {
+      return {};
+    }
+  }).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/hocs/withPrivateRoute/with-private-route.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {withPrivateRoute} from "./withPrivateRoute";
+
+const noop = () => {};
+
+const MockComponent = () => {
+
+  return (
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
+  );
+};
+const authorizationStatus = true;
+
+const MockComponentWrapped = withPrivateRoute(MockComponent, authorizationStatus);
+
+it(`withPrivateRoute is rendered correctly`, () => {
+  const tree = renderer.create((
+    <MockComponentWrapped />
+  ), {
+    createNodeMock() {
+      return {};
+    }
+  }).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
