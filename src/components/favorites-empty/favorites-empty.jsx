0f0be5be1bc2fbbbeffaed64ce2c2b0033d8d952
Aleks <osinaleksandr1989@gmail.com>
2020-11-13 13:52:08 +0500

    === Fixes markup mistake ===

--- src/components/favorites-empty/favorites-empty.jsx
+++ src/components/favorites-empty/favorites-empty.jsx
@@ -7,7 +7,7 @@ const FavoritesEmpty = () => {
         <h1 className="visually-hidden">FavoritesPage (empty)</h1>
         <div className="favorites__status-wrapper">
           <b className="favorites__status">Nothing yet saved.</b>
-          <p className="favorites__status-description">Save properties to narrow down search or plan yor future
+          <p className="favorites__status-description">Save properties to narrow down search or plan your future
             trips.</p>
         </div>
       </section>
