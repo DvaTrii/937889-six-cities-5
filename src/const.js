958e7bfe69ee9907206cd0a4f9077b7efd0e1ade
Aleks <osinaleksandr1989@gmail.com>
2020-11-16 15:21:50 +0500

    === Deletes unused consts ===

--- src/const.js
+++ src/const.js
@@ -12,17 +12,6 @@ export const AppRoute = {
   OFFER: `/offer/:id`,
 };
 
-export const ZOOM = 12;
-
-export const CitiesCoordinates = {
-  PARIS: [48.864716, 2.349014],
-  COLOGNE: [50.935173, 6.953101],
-  BRUSSELS: [50.8505, 4.3488],
-  AMSTERDAM: [52.38333, 4.9],
-  HAMBURG: [53.551086, 9.993682],
-  DUSSELDORF: [51.233334, 6.783333],
-};
-
 export const CardClass = {
   MAIN: `cities__place-`,
   NEAR: `near-places__`,
