85aa0acaaa5c9f27ce5f64978f2caa5178aa2e20
Aleks <osinaleksandr1989@gmail.com>
2020-11-18 22:41:55 +0500

    9.1.5 Adds useRef into LoginForm

--- src/components/login-form/login-form.jsx
+++ src/components/login-form/login-form.jsx
@@ -1,58 +1,55 @@
-import React, {createRef, PureComponent} from "react";
+import React, {useRef} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {login} from "../../store/user/operations";
 
-class LoginForm extends PureComponent {
-  constructor(props) {
-    super(props);
+const LoginForm = ({onSubmit}) => {
 
-    this.loginRef = createRef();
-    this.passwordRef = createRef();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const loginRef = useRef();
+  const passwordRef = useRef();
 
-  handleSubmit(evt) {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    this.props.onSubmit({
-      login: this.loginRef.current.value,
-      password: this.passwordRef.current.value,
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
     });
-  }
+  };
 
-  render() {
-    return (
-      <form
-        className="login__form form"
-        action="#"
-        method="post"
-        onSubmit={this.handleSubmit}>
-        <div className="login__input-wrapper form__input-wrapper">
-          <label className="visually-hidden">E-mail</label>
-          <input
-            ref={this.loginRef}
-            className="login__input form__input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required=""/>
-        </div>
-        <div className="login__input-wrapper form__input-wrapper">
-          <label className="visually-hidden">Password</label>
-          <input
-            ref={this.passwordRef}
-            className="login__input form__input"
-            type="password"
-            name="password"
-            placeholder="Password"
-            required="" />
-        </div>
-        <button className="login__submit form__submit button" type="submit">Sign in</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form
+      className="login__form form"
+      action="#"
+      method="post"
+      onSubmit={handleSubmit}>
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">E-mail</label>
+        <input
+          ref={loginRef}
+          className="login__input form__input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          required=""
+        />
+      </div>
+      <div className="login__input-wrapper form__input-wrapper">
+        <label className="visually-hidden">Password</label>
+        <input
+          ref={passwordRef}
+          className="login__input form__input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required=""
+        />
+      </div>
+      <button className="login__submit form__submit button" type="submit">Sign in</button>
+    </form>
+  );
+
+};
 
 LoginForm.propTypes = {
   onSubmit: PropTypes.func.isRequired
