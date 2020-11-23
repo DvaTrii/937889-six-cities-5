acb3234ef9f40e8ed22c1dadb7d77c85809be5c1
Aleks <osinaleksandr1989@gmail.com>
2020-11-23 14:27:14 +0500

    === Refactor: Rewrites withPrivateRoute into functional component ===

--- src/hocs/with-private-route/with-private-route.jsx
+++ src/hocs/with-private-route/with-private-route.jsx
@@ -3,17 +3,12 @@ import {Redirect} from "react-router-dom";
 import {AuthorizationStatus} from "../../const";
 
 export const withPrivateRoute = (Component, authorizationStatus, URL = `/`) => {
-  class PrivateRoute extends React.PureComponent {
-    constructor(props) {
-      super(props);
-    }
 
-    render() {
-      if (authorizationStatus === AuthorizationStatus.AUTH) {
-        return <Component {...this.props}/>;
-      } else {
-        return <Redirect to={URL}/>;
-      }
+  function PrivateRoute(props) {
+    if (authorizationStatus === AuthorizationStatus.AUTH) {
+      return <Component {...props}/>;
+    } else {
+      return <Redirect to={URL}/>;
     }
   }
 
