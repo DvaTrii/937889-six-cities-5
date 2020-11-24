ef035f134983cf72242614a8ccaabdcd510be6d4
Aleks <osinaleksandr1989@gmail.com>
2020-11-24 09:16:41 +0500

    === Refactor withPrivateRoute ===

--- src/hocs/with-private-route/with-private-route.jsx
+++ src/hocs/with-private-route/with-private-route.jsx
@@ -2,16 +2,9 @@ import React from "react";
 import {Redirect} from "react-router-dom";
 import {AuthorizationStatus} from "../../const";
 
-export const withPrivateRoute = (Component, authorizationStatus, URL = `/`) => {
-
-  function PrivateRoute(props) {
-    if (authorizationStatus === AuthorizationStatus.AUTH) {
-      return <Component {...props}/>;
-    } else {
-      return <Redirect to={URL}/>;
-    }
+export const withPrivateRoute = (Component, authorizationStatus, URL = `/`) => (props) => {
+  if (authorizationStatus === AuthorizationStatus.AUTH) {
+    return <Component {...props}/>;
   }
-
-  return PrivateRoute;
+  return <Redirect to={URL}/>;
 };
-
