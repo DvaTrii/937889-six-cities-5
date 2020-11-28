6ab7a957b9bc75a53f852fc75c324550699aebb8
Aleks <osinaleksandr1989@gmail.com>
2020-11-28 23:24:08 +0500

    === fixes const mistake / updates tests ===

--- src/const.js
+++ src/const.js
@@ -68,18 +68,18 @@ export const AuthorizationStatus = {
 export const REVIEWS_NUMBER = 10;
 
 export const Months = {
-  1: `January`,
-  2: `February`,
-  3: `March`,
-  4: `April`,
-  5: `May`,
-  6: `June`,
-  7: `July`,
-  8: `August`,
-  9: `September`,
-  10: `October`,
-  11: `November`,
-  12: `December`,
+  0: `January`,
+  1: `February`,
+  2: `March`,
+  3: `April`,
+  4: `May`,
+  5: `June`,
+  6: `July`,
+  7: `August`,
+  8: `September`,
+  9: `October`,
+  10: `November`,
+  11: `December`,
 };
 
 export const FavoritesRequestType = {
